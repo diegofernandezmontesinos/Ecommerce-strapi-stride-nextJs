be7a56dc64f14bc44145c12e28f66b6175c5fdfd
Diego <dfmontesinos@gmail.com>
2025-06-15 19:52:02 +0200

    Start ecommerce and settings

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,17 @@
-module.exports = () => ({});
+module.exports = () => ({
+    upload: {
+        config: {
+            provider:"aws-s3",
+            providerOptions: {
+                
+                acessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                secretAccessKey: process.env.AWS_ACCESS_SECRET,
+                region: process.env.AWS_REGION,
+                params: {
+                    Bucket: process.env.AWS_BUCKET,
+                    
+                },
+            }
+        }
+    }
+});
