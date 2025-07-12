930c3b58bc56dfef46dabcf131964cea5fe0f1e1
Diego <dfmontesinos@gmail.com>
2025-07-12 16:47:29 +0200

    Add prime react as component suites, instead of Semantic UI

--- ui/src/pages/_app.js
+++ ui/src/pages/_app.js
@@ -1,4 +1,7 @@
-import 'semantic-ui-css/semantic.min.css'
+import "primereact/resources/themes/lara-light-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
