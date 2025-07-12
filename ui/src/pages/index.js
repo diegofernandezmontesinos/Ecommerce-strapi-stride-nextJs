7182c8cf78e17e6eae54bc9fdedec5afced5fa31
Diego <dfmontesinos@gmail.com>
2025-07-12 17:11:42 +0200

    Add sass

--- ui/src/pages/index.js
+++ ui/src/pages/index.js
@@ -1,13 +1,9 @@
-import React from "react";
-import { Button } from 'primereact/button';
-
 const Index = () => {
   return (
     <div>
       <h2>Games Shop</h2>
-        <Button label="Click me" icon="pi pi-check" />
     </div>
   );
-}
+};
 
 export default Index;
