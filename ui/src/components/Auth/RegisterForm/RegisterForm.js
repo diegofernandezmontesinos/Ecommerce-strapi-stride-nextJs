b05427825c4eb46401fc36bae15365199d7822bc
Diego <dfmontesinos@gmail.com>
2025-07-20 09:06:58 +0200

    add formik and yup

--- ui/src/components/Auth/RegisterForm/RegisterForm.js
+++ ui/src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,30 +1,74 @@
-import React, { useState } from 'react';
-import { InputText } from 'primereact/inputtext';
-import { Button } from 'primereact/button';
-import styles  from './RegisterForm.module.scss';
+
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import styles from "./RegisterForm.module.scss";
+import  {useFormik} from "formik";
+import {  validationSchema, initialValues } from "./RegisterForm.form"
+
 
 export default function RegisterForm() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
 
-  const handleSubmit = () => {
-    e.preventDefault();
-    alert(`Nombre: ${name}\nEmail: ${email}`);
-  };
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    validateOnChange: false,
+    onSubmit:() => console.log("Formulario enviado"),
+  });
 
   return (
-    <form onSubmit={handleSubmit} className={styles.simpleForm}>
+    <form onSubmit={formik.handleSubmit} className={styles.simpleForm}>
       <div className={styles.formGroup}>
-        <label htmlFor="name">Nombre</label>
-        <InputText id="name" value={name} onChange={(e) => setName(e.target.value)} />
+        <label htmlFor="name">Nombre y apellido</label>
+        <InputText
+          id="name"
+          value={formik.values.name}
+          className={styles.inputText}
+          onChange={formik.handleChange}
+           error={formik.errors.email}
+        />
+      </div>
+
+       <div className={styles.formGroup}>
+        <label htmlFor="username">Usuario</label>
+        <InputText
+          id="username"
+          value={formik.values.username}
+          className={styles.inputText}
+          onChange={formik.handleChange}
+           error={formik.errors.username}
+        />
       </div>
 
       <div className={styles.formGroup}>
         <label htmlFor="email">Email</label>
-        <InputText id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <InputText
+          id="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          className={styles.inputText}
+           error={formik.errors.email}
+        />
       </div>
 
-      <Button label="Enviar" type="submit" className={styles.customSubmitButton} />
+      <div className={styles.formGroup}>
+        <label htmlFor="password">Contraseña</label>
+        <InputText
+          id="password"
+          type="password"
+          className={styles.inputText}
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.errors.password}
+
+        />
+      </div>
+
+      <Button
+        label="Enviar"
+        type="submit"
+        loading={formik.isSubmitting}
+        className={styles.customSubmitButton}
+      />
     </form>
   );
 }
