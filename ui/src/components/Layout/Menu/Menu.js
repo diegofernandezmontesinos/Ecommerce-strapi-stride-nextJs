eeae10b7e683e75d7bb35af6ffab32866dc7b52b
Diego <dfmontesinos@gmail.com>
2025-07-30 21:51:18 +0200

    search bar

--- ui/src/components/Layout/Menu/Menu.js
+++ ui/src/components/Layout/Menu/Menu.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 import { Platform } from "@/api";
+import { InputText } from "primereact/inputtext";
 import Link from "next/link";
 import { map } from "lodash";
 import styles from "./Menu.module.scss";
+import classNames from "classnames"
 import Image from "next/image";
 
 const platformCtrl = new Platform();
@@ -10,6 +12,9 @@ const platformCtrl = new Platform();
 export function Menu(props) {
   const { isOpenSearch } = props;
   const [platforms, setPlatforms] = useState(null);
+  const [value, setValue] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
+  const openCloseSearch = () => setShowSearch(prevState => !prevState)
 
   useEffect(() => {
     (async () => {
@@ -39,6 +44,33 @@ export function Menu(props) {
           {platform.title}
         </Link>
       ))}
+
+      <button
+        className={styles.search}
+        onClick={openCloseSearch}
+      >
+        <i className="pi pi-search" />
+      </button>
+
+      <div className={classNames(styles.inputContainer, {
+        [styles.active]: showSearch
+      })}>
+        <span className="p-input-icon-left">
+          <i className="pi pi-search" />
+          <InputText
+            value={value}
+            className={styles.input}
+            onChange={(e) => setValue(e.target.value)}
+            placeholder="Buscar..."
+          />
+          <button
+            className={styles.closeInput}
+            onClick={openCloseSearch}
+          >
+            <i className="pi pi-times" name="close" />
+          </button>
+        </span>
+      </div>
     </div>
   );
 }
